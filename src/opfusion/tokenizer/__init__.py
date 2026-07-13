37c174d53e729787834fd25ac4016e4dd06415a5
Unjuno <unjuno.org@unjuno.org>
2026-07-14 00:01:39 +0900

    Export fixed experiment tokenizer

--- src/opfusion/tokenizer/__init__.py
+++ src/opfusion/tokenizer/__init__.py
@@ -1,10 +1,13 @@
 from .build_vocab import build_vocab, build_vocab_hash, build_vocab_map
+from .fixed import FixedVocabTokenizer, TokenizerMetadata
 from .reserved import build_output_allow_list, is_reserved_operator_token, reserved_operator_tokens
 
 __all__ = [
     "build_vocab",
     "build_vocab_map",
     "build_vocab_hash",
+    "FixedVocabTokenizer",
+    "TokenizerMetadata",
     "build_output_allow_list",
     "is_reserved_operator_token",
     "reserved_operator_tokens",
